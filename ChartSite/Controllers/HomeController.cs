de8e3bf6172da90fe58324aaa91cc7c334cba357
Karol Horodelski <karol.horodelski@contractors.roche.com>
2017-05-20 12:51:49 +0200

    Dodanie logiki akcji Index

--- ChartSite/Controllers/HomeController.cs
+++ ChartSite/Controllers/HomeController.cs
@@ -23,7 +23,35 @@ namespace ChartSite.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var chart = new ChartData<BarChartDataset>();
+
+            var dataset = new BarChartDataset();
+
+            dataset.Data.Add(5);
+            dataset.Data.Add(3);
+            dataset.Data.Add(4);
+            dataset.Data.Add(7);
+
+            dataset.BackgroundColor.Add(Color.Red);
+            dataset.BackgroundColor.Add(Color.Green);
+            dataset.BackgroundColor.Add(Color.Blue);
+            dataset.BackgroundColor.Add(Color.Yellow);
+            
+
+            dataset.Label = "Kolory";
+
+            chart.Datasets.Add(dataset);
+
+            chart.Labels.Add("Czerwony");
+            chart.Labels.Add("Zielony");
+            chart.Labels.Add("Niebieski");
+            chart.Labels.Add("Zolty");
+
+            var model = new IndexViewModel();
+
+            model.ChartData = SerializationHelpers.ToJson(chart);
+
+            return View(model);
         }
 
         public ActionResult Lifespan()
